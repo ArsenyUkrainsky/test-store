3df699931983eeeff57fa302e651e25c13faa804
Arseny Ukrainsky <ArseniyUkrainskiy@yandex.ru>
2021-10-25 16:53:35 +0300

    проект настроен TypeScript, Redux, Mobx, Eslint, Stylelint

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-import "./App.css";
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  const testButton = () => setCount(x => x +1)
+  const [count, setCount] = useState(0)
+  const testButton = () => setCount((x) => x + 1)
   return (
-    <div className="App">
+    <div className='app'>
       <h1>Test</h1>
       <p>{count}</p>
-      <button onClick={testButton}>прибавить</button>
+      <button type='button' onClick={testButton}>
+        прибавить
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
