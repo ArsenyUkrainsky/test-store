89cf4d4053ce2885d4ae850a0fd7ef4862d79c39
Arseny Ukrainsky <ArseniyUkrainskiy@yandex.ru>
2021-11-03 15:47:53 +0300

    +2 кнопки на главную

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 import Counter from '../Counter/Counter'
 import Page1 from '../Page1/Page1'
 import Page2 from '../Page2/Page2'
@@ -8,6 +8,14 @@ import './App.css'
 
 const App = () => (
   <div className='app'>
+    <nav>
+      <Link to='/rX' className='button' style={{ backgroundImage: 'linear-gradient(to left, #4880EC, #df5d18)', borderRadius: '40px 20px', margin: '35px' }}>
+        Redux
+      </Link>
+      <Link to='/mX' className='button' style={{ backgroundImage: 'linear-gradient(to right, #4880EC, #df5d18)', borderRadius: '20px 40px', margin: '35px' }}>
+        MobX
+      </Link>
+    </nav>
     <Switch>
       <Route exact path='/rX'>
         <Page1 />
